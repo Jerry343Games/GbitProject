776d0ac1946a57a76fa6817ce743a6e3b2c52d9f
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2023-07-13 23:58:04 +0800

    new:导入DoTween工具

--- Program/Assets/Scripts/Player/PlayerController.cs
+++ Program/Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
