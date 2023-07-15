07900643dc985f41d1aa1701e88b645b150cd4bb
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2023-07-15 16:36:38 +0800

    导入DoTween

---
+++ Program/Assets/Scripts/UICheck.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UICheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Check()
+    {
+        
+    }
+}
