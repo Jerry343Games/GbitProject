2fe0a4ccd04ab8cf1f1f82d0eded59d5ffc8d9bd
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2023-07-15 13:41:42 +0800

    导包和相机震动脚本

---
+++ Program/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private float shakeDuration = 0f;
+    private float shakeMagnitude = 0.1f;
+    private float dampingSpeed = 1.0f;
+
+    void Awake()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = originalPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = originalPosition;
+        }
+    }
+
+    /// <summary>
+    /// 相机震动方法
+    /// </summary>
+    /// <param name="duration">震动时间</param>
+    /// <param name="magnitude">震动幅度</param>
+    public void Shake(float duration, float magnitude)
+    {
+        shakeDuration = duration;
+        shakeMagnitude = magnitude;
+    }
+}
